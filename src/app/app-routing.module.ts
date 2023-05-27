ce55fb7ea1d6374bc9c0d330836f622a415998ef
Mohamed Kamel <admin@cvrest.com>
2023-05-27 20:12:32 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { BoardComponent } from './board-component/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mainpage', component: MainComponent },
+  { path: 'iframepage', component: BoardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NgxChessBoardModule } from "ngx-chess-board";
+import { BoardComponent } from './board-component/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MainComponent, BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChessBoardModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
